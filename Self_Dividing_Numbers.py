d488aa4ba2e3403e4f23a3b9a66f0748e730fd0e
gvsaisurya02 <98895387+gvsaisurya02@users.noreply.github.com>
2022-10-03 15:14:25 +0530

    n=int(input()) m=int(input()) for i in range(n,m+1):     flag=0     l=[]     temp=i     while i>0:         r=i%10         l.append(r)         i=i//10     for j in l:         if j!=0:             if temp%j!=0:                 flag=1         else:             flag=1     if flag==0:         print(temp,end=" ")

--- Self_Dividing_Numbers.py
+++ Self_Dividing_Numbers.py
@@ -1,6 +1,7 @@
 n=int(input())
 m=int(input())
 for i in range(n,m+1):
+    flag=0
     l=[]
     temp=i
     while i>0:
@@ -8,9 +9,11 @@ for i in range(n,m+1):
         l.append(r)
         i=i//10
     for j in l:
-        if j>0:
-            if temp%j!=0 or temp%10==0:
-                break
-    else:
+        if j!=0:
+            if temp%j!=0:
+                flag=1
+        else:
+            flag=1
+    if flag==0:
         print(temp,end=" ")
         
