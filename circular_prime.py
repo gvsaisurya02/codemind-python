4eba047b8fc8740198d39638bc881517d7b69245
gvsaisurya02 <98895387+gvsaisurya02@users.noreply.github.com>
2022-10-13 22:08:33 +0530

    n=input() rev=n[::-1] n=int(n) rev=int(rev) flag,flag1=0,0 for i in range(2,n):     if n%i==0:         break else:     flag=1 for j in range(2,rev):     if rev%j==0:         break else:     flag1=1 if flag==1 and flag1==1:     print("circular prime") elif flag==1 and flag1==0:     print("prime but not a circular prime") elif flag==0 and flag1==0:     print("not prime")

--- circular_prime.py
+++ circular_prime.py
@@ -1,16 +1,21 @@
-n=int(input())
-n=str(n)
-p,rp=0,0
-r=n[::-1]
-for i in range(2,int(n)):
-    if int(n)%i==0:
-        p=1
-for i in range(2,int(r)):
-    if int(r)%i==0:
-        rp=1
-if p==0 and rp==0:
-    print("circular prime")
-elif p==0 and rp!=0:
-    print("prime but not a circular prime")
+n=input()
+rev=n[::-1]
+n=int(n)
+rev=int(rev)
+flag,flag1=0,0
+for i in range(2,n):
+    if n%i==0:
+        break
 else:
+    flag=1
+for j in range(2,rev):
+    if rev%j==0:
+        break
+else:
+    flag1=1
+if flag==1 and flag1==1:
+    print("circular prime")
+elif flag==1 and flag1==0:
+    print("prime but not a circular prime")
+elif flag==0 and flag1==0:
     print("not prime")
