df3430c952c617e0779d94ed1eea5c13e7a239c4
gvsaisurya02 <98895387+gvsaisurya02@users.noreply.github.com>
2022-11-04 15:01:43 +0530

    n=int(input()) for i in range(n):     for j in range(1,n-1):         print(j,end="")     for j in range(1,n-2):         print(j,end="")     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,6 +1,7 @@
 n=int(input())
-l=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O']
 for i in range(n):
-    for j in range(n):
-        print(l[i],end=" ")
+    for j in range(1,n-1):
+        print(j,end="")
+    for j in range(1,n-2):
+        print(j,end="")
     print()
